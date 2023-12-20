f948a0d8afad34357d225b1218262f1f1d82db49
ntx7 <89039787+ntx7@users.noreply.github.com>
2023-12-21 01:14:43 +0500

    Update site.js

--- site.js
+++ site.js
@@ -18,43 +18,20 @@ function createRandomWords() {
 
             // Force reflow before adding class for transition
             word.offsetHeight;
-
-            word.style.opacity = 1;
-
-            const speed = 0.2 + parseFloat(word.style.fontSize.replace('px', '')) / 100; // Уменьшенная скорость, зависящая от размера
-
-            function moveWord() {
-                const topPosition = parseFloat(word.style.top.replace('px', ''));
-                word.style.top = `${topPosition + speed}px`;
-
-                if (topPosition < window.innerHeight) {
-                    requestAnimationFrame(moveWord);
-                } else {
-                    wordContainer.removeChild(word);
-                }
-            }
-
-            moveWord();
         }, i * 400); // Увеличивающаяся задержка между словами
     }
 }
 
 function addCenterText() {
     const centerText = document.createElement('div');
+    centerText.id = 'center-text';
     centerText.textContent = 'я тебя люблю';
-    centerText.style.fontSize = '24px';
-    centerText.style.position = 'absolute';
-    centerText.style.top = '50%';
-    centerText.style.left = '50%';
-    centerText.style.transform = 'translate(-50%, -50%)';
-    centerText.style.color = 'white';
-    centerText.style.textShadow = '2px 2px 4px rgba(0, 0, 0, 0.8)'; // Добавляем тень
     wordContainer.appendChild(centerText);
 }
 
 function generateWords() {
     addCenterText();
-    setInterval(createRandomWords, 6000); // Общая задержка между сериями слов
+    createRandomWords();
 }
 
 generateWords();
